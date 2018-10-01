2b345739fcba39b8a28f36d39648257037ff1399
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2018-10-01 22:12:19 +0200

    Added libraries

--- arduino/matrix-clock.ino
+++ arduino/matrix-clock.ino
@@ -1,11 +1,35 @@
 #include <ESP8266WiFi.h>
 
-void setup() {
+#include <Time.h>
+#include <TimeLib.h>
+#include <Timezone.h>
 
+#include <Wire.h>
+
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+WiFiUDP ntpUDP;
+NTPClient::Client timeClient(ntpUDP);
+
+void setup()
+{ 
+  // Init serial port
+  Serial.begin(115200);
+
+  // Init display
+
+  // Init I2C
+
+  // connect to WiFi
+
+  // Start NTP client
+  timeClient.begin();   // Start the NTP UDP client
 }
 
+void loop()
+{
+  Serial.println('.');
 
-void loop() {
-  
+  delay(1000);
 }
-
