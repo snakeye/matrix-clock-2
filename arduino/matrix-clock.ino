66e910e8da824cdfcd62f76c189459d491c7e804
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2018-10-02 20:34:35 +0200

    Added WiFi connection

--- arduino/matrix-clock.ino
+++ arduino/matrix-clock.ino
@@ -9,27 +9,68 @@
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 
+#include "src/conf.h"
+
+#define ONBOARDLED 2
+
 WiFiUDP ntpUDP;
 NTPClient::Client timeClient(ntpUDP);
 
+const byte interruptPin = 12;
+
+void onSTAConnected(WiFiEventStationModeConnected ipInfo)
+{
+  Serial.printf("Connected to %s\r\n", ipInfo.ssid.c_str());
+}
+
+// Start NTP only after IP network is connected
+void onSTAGotIP(WiFiEventStationModeGotIP ipInfo)
+{
+  Serial.printf("Got IP: %s\r\n", ipInfo.ip.toString().c_str());
+  Serial.printf("Connected: %s\r\n", WiFi.status() == WL_CONNECTED ? "yes" : "no");
+  digitalWrite(ONBOARDLED, LOW); // Turn on LED
+}
+
+// Manage network disconnection
+void onSTADisconnected(WiFiEventStationModeDisconnected event_info)
+{
+  Serial.printf("Disconnected from SSID: %s\n", event_info.ssid.c_str());
+  Serial.printf("Reason: %d\n", event_info.reason);
+  digitalWrite(ONBOARDLED, HIGH); // Turn off LED
+}
+
 void setup()
-{ 
+{
+  static WiFiEventHandler e1, e2, e3;
+
   // Init serial port
   Serial.begin(115200);
+
+  //
+  pinMode(ONBOARDLED, OUTPUT);    // Onboard LED
+  digitalWrite(ONBOARDLED, HIGH); // Switch off LED
 
   // Init display
 
   // Init I2C
 
+  //
+  pinMode(interruptPin, INPUT_PULLUP);
+
   // connect to WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_network, wifi_password);
+
+  e1 = WiFi.onStationModeGotIP(onSTAGotIP); // As soon WiFi is connected, start NTP Client
+  e2 = WiFi.onStationModeDisconnected(onSTADisconnected);
+  e3 = WiFi.onStationModeConnected(onSTAConnected);
 
   // Start NTP client
-  timeClient.begin();   // Start the NTP UDP client
+  timeClient.begin(); // Start the NTP UDP client
 }
 
 void loop()
 {
-  Serial.println('.');
 
   delay(1000);
 }
---
+++ arduino/src/conf.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+extern const char* wifi_network;
+extern const char* wifi_password;
+
+#endif
