a684e0b5313ba0ff271e3d38de810a3d527cfeab
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2021-02-23 08:59:36 +0100

    Ambient light sensor

---
+++ firmware/ambient_light_sensor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "esphome.h"
+
+class AmbientLightSensor : public PollingComponent, public Sensor {
+ public:
+  // constructor
+  AmbientLightSensor() : PollingComponent(10000) {}
+
+  float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
+
+  void setup() override {
+    // This will be called by App.setup()
+  }
+  void update() override {
+    publish_state((analogRead(A0) * 15) / 1024);
+  }
+};
